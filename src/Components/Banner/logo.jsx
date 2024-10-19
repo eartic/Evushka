1eb7f995e7e448423d46bcfdf7d51e6df70139c2
eartic <evanartic17@gmail.com>
2024-10-19 09:57:39 +0200

    pomaknut div

--- src/Components/Banner/logo.jsx
+++ src/Components/Banner/logo.jsx
@@ -5,12 +5,12 @@
     return (
       <>
         <div className="relative flex justify-center items-center m-2 p-3">
-          {/* Positioned text */}
-          <p className="absolute xl:top-8 xl:right-1/4 text-black text-sm md:text-base sm:text-xs font-semibold z-10 sm:right-2 sm:top-2 md:right-4 md:top-5">
+          
+          <p className="absolute top-4 right-6 xl:top-8 xl:right-1/4 text-black text-sm md:text-base sm:text-xs font-semibold z-10  md:right-4 md:top-5">
             Plesna igraonica & rođendaonica
           </p>
           
-          {/* Centered image */}
+         
           <img src={LogoImage} alt="logo banner" className="max-w-xs sm:max-w-sm md:max-w-md h-auto xl:max-w-sm" />
         </div>
       </>
