6474ebba6f7cb488012ee3c722617af6f53e9846
eartic <evanartic17@gmail.com>
2024-10-19 10:04:03 +0200

    div centriran

--- src/Components/Banner/logo.jsx
+++ src/Components/Banner/logo.jsx
@@ -6,7 +6,7 @@
       <>
         <div className="relative flex justify-center items-center m-2 p-3">
           
-          <p className="absolute top-4 right-6 xl:top-8 xl:right-1/4 text-black text-sm md:text-base sm:text-xs font-semibold z-10  md:right-4 md:top-5">
+          <p className="absolute top-4 right-6 xl:top-8 xl:right-1/3 text-black text-sm md:text-base sm:text-xs font-semibold z-10  md:right-4 md:top-5">
             Plesna igraonica & rođendaonica
           </p>
           
